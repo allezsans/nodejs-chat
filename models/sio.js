75e59b6a879296f570a128a1107d4b09fae04e9a
allezsans <allez.sans.question@gmail.com>
2015-10-03 19:23:47 +0900

    送信者にも送信内容を通知する

--- models/sio.js
+++ models/sio.js
@@ -30,7 +30,7 @@ function sio(server) {
 		socket.on('notice', function(data) {
 			// すべてのクライアントへ通知を送信
 			// ブロードキャスト
-			socket.broadcast.emit('recieve', {
+			socket.sockets.emit('recieve', {
 				type : data.type,
 				user : data.user,
 				value : data.value,
